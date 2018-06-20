8bb653413f4c2e4a628b978b40d7c2158da2bc50
dhjung <dahae.jung@atto-research.com>
2018-06-20 17:32:50 +0900

    web-02: jquery

--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -1,9 +1,19 @@
 /**
  * Created by dhjung on 2018. 6. 20..
  */
-window.onload = function () {
-    document.getElementById("button1").onclick = function () {
-        document.getElementById("div1").innerHTML = "Hello JAVASCRIPT";
-    }
+// window.onload = function () {
+//     document.getElementById("button1").onclick = function () {
+//         document.getElementById("div1").innerHTML = "Hello JAVASCRIPT";
+//     }
+//
+// }
 
-}
+$(document).ready(function(){
+    var a = $('input').attr("value");
+    console.log(a);
+
+    var b = $('input').attr("type");
+    console.log(b);
+
+    $('input').attr("value", "변경한 값입니다");
+});
