db5b6c2712b58211dc4bbe8f661c9c0ec6725485
dhjung <dahae.jung@atto-research.com>
2018-06-20 17:46:56 +0900

    web-03: jquery vs javascript

--- build/resources/main/public/js/app.js
+++ build/resources/main/public/js/app.js
@@ -1,6 +1,7 @@
 /**
  * Created by dhjung on 2018. 6. 20..
  */
+// web-01
 // window.onload = function () {
 //     document.getElementById("button1").onclick = function () {
 //         document.getElementById("div1").innerHTML = "Hello JAVASCRIPT";
@@ -8,12 +9,27 @@
 //
 // }
 
-$(document).ready(function(){
-    var a = $('input').attr("value");
-    console.log(a);
+// web-02
+// $(document).ready(function(){
+//     var a = $('input').attr("value");
+//     console.log(a);
+//
+//     var b = $('input').attr("type");
+//     console.log(b);
+//
+//     $('input').attr("value", "변경한 값입니다");
+// });
 
-    var b = $('input').attr("type");
-    console.log(b);
+// web-03
+var temp ="";
+window.onload = function () {
+    document.getElementById("btn1").onclick = function () {
+        document.getElementById("result").innerHTML = document.getElementById("input1").value;
+    }
+}
 
-    $('input').attr("value", "변경한 값입니다");
+$(document).ready(function () {
+    $("#btn2").click(function () {
+        $("#result").text($("#input1").val());
+    });
 });
